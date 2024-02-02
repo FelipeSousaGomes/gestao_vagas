df7fa00f764ba1872d34404df1d4d03d5bac9239
Felipe <felipesouzagomes21@gmail.com>
2024-02-02 00:09:53 -0300

    Formatando mensagem de validação

--- src/main/java/br/com/felipe/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/felipe/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -15,12 +16,13 @@ public class CandidateEntity {
     private UUID id;
     private String name;
 
-    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo não aceita espaço")
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo não aceita espaço")
     private String username;
     @Email(message = "O campo email deve conter um e-mail válido")
     private String email;
 
-    @Length(min = 10, max = 100)
+    @Length(min = 10, max = 100, message = "o campo deve ser de 10 a 100")
     private String password;
     private String curriculum;
 
